38277918636573d347e321035b311323965b6bbe
jha-nisha <nisha.jha11march@gmail.com>
2024-07-19 13:48:44 +0530

    Add files via upload

---
+++ AuthorizationDemo/src/test/java/com/authorization/AuthorizationDemo/AuthorizationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authorization.AuthorizationDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
